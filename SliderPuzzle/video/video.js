ea9befbd9e0aaba0c3460c0c76458f34846ca4e7
ksVPN13 <jacob.gray13@gmail.com>
2021-11-01 21:00:47 -0500

    updated video puzzle

--- SliderPuzzle/video/video.js
+++ SliderPuzzle/video/video.js
@@ -1,22 +1,36 @@
+/* Slider Puzzle - Customizable*/
 
-const rows = 5;
-const columns = 5;
-let pWidth;
-let pHeight;
-const imgSrc = 'https://preview.redd.it/mw4e64kb27w71.gif?width=640&format=mp4&s=7c9f28915abf0a41516a17f0250efc985dea8fde';
+//declare and set initial variables
+let rows = 4;
+let columns = 4;
+const pWidth = 100;
+const pHeight = 100;
+let imgSrc = 'https://preview.redd.it/mw4e64kb27w71.gif?width=640&format=mp4&s=7c9f28915abf0a41516a17f0250efc985dea8fde';
 let winCondition;
 let gameOver = false;
 let clicks = 0;
+let syncIndex = 1;
 
-//set width and heigh values based on screen type
-if (screen.width < 450 ) {
-    pWidth = 95;
-    pHeight = 80;
-    //console.log('mobile');
-} else {
-    pWidth = 85;
-    pHeight = 80;
-    //console.log('desktop');
+//get elements from webpage
+const rSlider = document.getElementById('rows');
+const cSlider = document.getElementById('cols');
+const rVal = document.getElementById('rVal');
+const cVal = document.getElementById('cVal');
+const imgUrl = document.getElementById('url');
+const form = document.getElementById('form');
+const footer = document.getElementById('footer');
+const submitButton = document.getElementById('submit');
+const titleText = document.getElementById('titleText');
+const localFile = document.getElementById('local');
+const clickCount = document.getElementById('click');
+let allVideos;
+
+// Update the current slider value (each time you drag the slider handle)
+rSlider.oninput = function() {
+    rVal.innerHTML = this.value;
+  }
+cSlider.oninput = function() {
+    cVal.innerHTML = this.value;
 }
 
 //creates the puzzle
@@ -25,7 +39,11 @@ function createPuzzle(row, col) {
     for (let i=0; i<row; i++) {
         createRow(i, col);
     }
+    makeClickable();
     winCondition = Array.from(document.getElementsByClassName('image'));
+    allVideos = document.getElementsByClassName('image');
+    shuffle();
+    setTimeout(playVids, 5000);
 }
 
 //takes row number and a count of columns to create the rows
@@ -35,7 +53,8 @@ function createRow(index, col) {
     let rDiv = document.createElement('div');
     //set id of element to r#
     rDiv.id = 'r' + index;
-    rDiv.className = 'row';
+    rDiv.className = 'row'
+    rDiv.style.height = (pHeight/rows) + '%';
     //add rDiv to puzzle
     pDiv.appendChild(rDiv);
 
@@ -48,37 +67,38 @@ function createRow(index, col) {
 //takes a row element to append slide elements based on row number and column number
 function createSlide(rowDiv, rowNum, colNum) {
     //calculate image offset for slide
-    let xMargin = -colNum*(pWidth/columns);
-    let yMargin = -rowNum*(pHeight/rows);
+    let xMargin = -colNum*(document.body.clientWidth/columns);
+    let yMargin = -rowNum*(document.body.clientHeight/rows);
 
     //create slide container element
     let sDiv = document.createElement('div');
     sDiv.className = 'slide';
     sDiv.id = 'r' + rowNum + 's' + colNum;
-    sDiv.style.width = (pWidth/columns) + 'vw';
-    sDiv.style.height = (pHeight/rows) + 'vh';
+    sDiv.style.width = (pWidth/columns) + '%';
     rowDiv.appendChild(sDiv);
 
     //create slide image element
-    //let sImg = document.createElement('img');
     let sImg = document.createElement('VIDEO');
-    sImg.src = imgSrc;
-    //sImg.autoplay = true;
-    sImg.muted = true;
-    sImg.loop = true;
-    sImg.className = 'image';
-    sImg.style.objectFit = 'cover';
-    sImg.style.width = pWidth + 'vw';
-    sImg.style.height = pHeight + 'vh';
-    //margins offset the image to show the right piece for each slide
-    sImg.style.margin = yMargin + 'vh 0 0 ' + xMargin + 'vw';
-
+    //sets first slide id to be 'blank' and make it invisible, but still clickable
     if (rowNum === 0 && colNum === 0) {
         sImg.id = 'blank';
         sImg.style.opacity = 0;
     } else {
         sImg.id = 'r' + rowNum + 'i' + colNum;
     }
+    sImg.src = imgSrc;
+    //set image size to take up the full screen
+    //sImg.autoplay = true;
+    sImg.muted = true;
+    sImg.loop = true;
+    sImg.className = 'image';
+    sImg.style.objectFit = 'cover';
+    sImg.style.width = document.body.clientWidth + 'px';
+    sImg.style.height = document.body.clientHeight + 'px';
+
+    //margins offset the image to show the right piece for each slide
+    sImg.style.margin = yMargin + 'px 0 0 ' + xMargin + 'px';
+
     //add slide image to container element
     sDiv.appendChild(sImg);
 }
@@ -138,7 +158,7 @@ function move(sCont) {
             }
         }
     } else {
-        return; //stops from counting click
+        return; //stops from counting click when a non-moving slide is clicked
     }
     countClick();
     //console.log(sElem.item(i));
@@ -159,36 +179,31 @@ function checkWin() {
     let currentLayout = Array.from(document.getElementsByClassName('image'));
     //console.log(currentLayout);
     //console.log(winCondition);
-    if (isEqual(currentLayout, winCondition)) {
-        //if win condition is met, change text, stop clicks and remove margin from slides
-        document.getElementById('title').innerHTML = '<h1>You Win!</h1>';
-        console.log('You win!');
+    if (isEqual(currentLayout, winCondition) && (!gameOver)) {
+        //if win condition is met, show new game button, stop clicks, and remove borders from slides
+        //document.getElementById('title').innerHTML = '<h1>You Win!</h1>';
+        //console.log('You win!');
         document.getElementById('blank').style.opacity = 1;
         gameOver = true;
 
-        //remove blank and replace with correct slide
-        /*document.getElementById('blank').remove();
-        let sImg = document.createElement('VIDEO');
-        sImg.id = 'first';
-        sImg.src = imgSrc;
-        sImg.autoplay = true;
-        sImg.muted = true;
-        sImg.loop = true;
-        sImg.className = 'image';
-        sImg.style.objectFit = 'cover';
-        sImg.style.width = pWidth + 'vw';
-        sImg.style.height = pHeight + 'vh';
-        //margins offset the image to show the right piece for each slide
-        sImg.style.margin = '0 0 0 0';
-        //add slide image to container element
-        document.getElementById('r0s0').appendChild(sImg);
-        */
-
-        //loop through slides to remove margin
+        //loop through slides to remove border
+        /*
         let sElem = document.getElementsByClassName('slide');
         for (let i = 0; i < sElem.length; i++) {
-            sElem.item(i).style.margin = '0px';
-        }
+            sElem.item(i).style.border = 'none';
+        }*/
+
+        //create play again button to refresh page
+        let playAgain = document.createElement('div');
+        playAgain.className = 'button';
+        playAgain.id = 'reload';
+        playAgain.style.display = 'inline-block';
+        playAgain.style.fontSize = '20px';
+        playAgain.innerHTML = 'Play Again';
+        document.getElementById('youWin').appendChild(playAgain);
+
+        //clicking the button just reloads the web page
+        playAgain.addEventListener('click', () => location.reload());
     }
 }
 
@@ -201,10 +216,10 @@ function shuffle() {
     }
 
     clicks = 0;
-    document.getElementById('click').innerHTML = clicks;
+    clickCount.innerHTML = clicks;
 }
 
-//moves a random slide, only picking one that is moveable
+//moves a random slide, only picking one that is moveable; still can pick blank..
 function moveRandomSlide() {
     //get the blank location
     let blank = document.getElementById('blank');
@@ -218,9 +233,11 @@ function moveRandomSlide() {
     //selects a random slide and moves it
     if (ranBox < rows) {
         ranSlide = document.getElementById('r' + ranBox + 's' + blankCol);
+        //console.log(ranSlide);
         move(ranSlide);
     } else {
         ranSlide = document.getElementById('r' + blankRow + 's' + (ranBox-rows));
+        //console.log(ranSlide);
         move(ranSlide);
     }
     //console.log(ranBox);
@@ -234,35 +251,86 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
 
-//takes the click event and add the move function to the item that was clicked
+//takes the click event and adds the move function to the item that was clicked
 function userMove(event) {
     //console.log(event);
     move(event.currentTarget);
     checkWin();
 }
 
+//adds click events to all slides
+function makeClickable() {
+    let sElem = document.getElementsByClassName('slide');
+    for (let i = 0; i < sElem.length; i++) {
+        sElem.item(i).addEventListener("click", userMove);
+    }
+}
+
 //counts and upates clicks
 function countClick() {
     clicks++;
-    document.getElementById('click').innerHTML = clicks;
+    clickCount.innerHTML = clicks;
 }
 
 //try to play videos at the same time
 function playVids(){
-    let sElem = document.getElementsByClassName('image');
-    for (let i = 0; i < sElem.length; i++) {
-        sElem.item(i).play();
+    for (let i = 0; i < allVideos.length; i++) {
+        allVideos.item(i).currentTime = allVideos.item(0).currentTime;
+        allVideos.item(i).play();
+    }
+    setInterval(syncVidsV1, 500);
+    //setInterval(syncVidsV2, 5000);
+    //setTimeout(syncVidsV2,3000);
+}
+
+//this function needs to be called on an interal to sync each slide one at a time
+function syncVidsV1(){
+    if (syncIndex < allVideos.length) {
+        allVideos.item(syncIndex).currentTime = allVideos.item(0).currentTime
+        syncIndex++;
+    } else {
+        syncIndex = 1;
     }
 }
 
-//initialize puzzle and shuffle
-createPuzzle(rows, columns);
-shuffle();
-
-setTimeout(playVids, 5000);
-
-//adds click events to all slides
-let sElem = document.getElementsByClassName('slide');
-for (let i = 0; i < sElem.length; i++) {
-    sElem.item(i).addEventListener("click", userMove);
+//this function needs to be called on an interal to sync each slide one at a time
+function syncVidsV2(){
+    //allVideos.item(0).pause();
+    for (let i = 0; i < allVideos.length; i++) {
+        allVideos.item(i).pause();
+        allVideos.item(i).currentTime = allVideos.item(0).currentTime;
+    }
+    playVids();
 }
+
+//initialize puzzle and shuffle
+//createPuzzle(rows, columns);
+//shuffle();
+
+//generates puzzle based on input in the webpage
+submitButton.addEventListener('click', () => {
+    rows = parseInt(rSlider.value);
+    columns = parseInt(cSlider.value);
+    imgSrc = imgUrl.value;
+
+    const files = document.getElementById('local').files[0];
+    //console.log(files);
+
+    //if custom image was selected, load then generate puzzle
+    if (files) {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(files);
+      fileReader.addEventListener("load", function () {
+        imgSrc = this.result;
+        //console.log('image loaded');
+        createPuzzle(rows, columns);
+      });
+    } else {
+        createPuzzle(rows, columns);
+    }
+
+    //remove form and title from page and add click count
+    footer.style.visibility = 'visible';
+    form.remove();
+    titleText.remove();
+});
